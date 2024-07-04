aa2eb833964c4a57b85f3d7cbc8e2fd82a52aacd
Filipe Ferreira <filipeferreira96@hotmail.com>
2024-07-04 17:56:42 +0100

    tsx fix

--- client/src/components/footer/mobile.tsx
+++ client/src/components/footer/mobile.tsx
@@ -153,7 +153,7 @@ export function MobileFooter() {
       setActive(index);
     }
     if (data[index].url) {
-      router.push(data[index].url);
+      router.push(data[index].url as string);
     } else {
       data[index]?.onClick();
     }
