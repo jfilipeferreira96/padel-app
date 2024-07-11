032b51650c7ca4255c2d3e4087ae58fb5d5936cc
Filipe Ferreira <filipeferreira96@hotmail.com>
2024-07-11 12:35:53 +0100

    image click

--- client/src/app/(with-auth)/(players)/home/page.tsx
+++ client/src/app/(with-auth)/(players)/home/page.tsx
@@ -1,33 +1,31 @@
 "use client";
 import React, { useState, useRef, useEffect } from "react";
-import { Center, Card, Loader, Title, SimpleGrid, Image, Text, Container, AspectRatio } from "@mantine/core";
+import { Center, Card, Loader, Title, SimpleGrid, Image, Text, Container, AspectRatio, Box } from "@mantine/core";
 import classes from "./classes.module.css";
 import { useSession } from "@/providers/SessionProvider";
+import "photoswipe/dist/photoswipe.css";
+import { Gallery, Item } from "react-photoswipe-gallery";
 
 const mockdata = [
   {
-    title: 'Top 10 places to visit in Norway this summer',
-    image:
-      'https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-    date: 'August 18, 2022',
+    title: "Top 10 places to visit in Norway this summer",
+    image: "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    date: "August 18, 2022",
   },
   {
-    title: 'Best forests to visit in North America',
-    image:
-      'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-    date: 'August 27, 2022',
+    title: "Best forests to visit in North America",
+    image: "https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    date: "August 27, 2022",
   },
   {
-    title: 'Hawaii beaches review: better than you think',
-    image:
-      'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-    date: 'September 9, 2022',
+    title: "Hawaii beaches review: better than you think",
+    image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    date: "September 9, 2022",
   },
   {
-    title: 'Mountains at night: 12 best locations to enjoy the view',
-    image:
-      'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-    date: 'September 12, 2022',
+    title: "Mountains at night: 12 best locations to enjoy the view",
+    image: "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    date: "September 12, 2022",
   },
 ];
 
@@ -42,18 +40,35 @@ function Home() {
     );
   }
 
-  const cards = mockdata.map((article) => (
-    <Card key={article.title} p="md" radius="md" component="a" href="#" className={classes.card}>
-      <AspectRatio ratio={1920 / 1080}>
-        <Image src={article.image} alt={article.title} />
-      </AspectRatio>
-      <Text c="dimmed" size="xs" tt="uppercase" fw={700} mt="md">
-        {article.date}
-      </Text>
-      <Text className={classes.title} mt={5}>
-        {article.title}
-      </Text>
-    </Card>
+  const cards = mockdata.map((article, index) => (
+    <Gallery key={index}>
+      <Item
+        content={
+          <Center h={"100%"} bg="var(--mantine-color-gray-light)" p={20}>
+            <div>
+              <Title className={classes.title} ta="center" mb={"lg"}>
+                {article.title}
+              </Title>
+              <Image src={article.image} alt={article.title} />
+            </div>
+          </Center>
+        }
+      >
+        {({ ref, open }) => (
+          <Card p="md" radius="md" component="a" href="#" className={classes.card} ref={ref} onClick={open}>
+            <AspectRatio ratio={1920 / 1080}>
+              <Image src={article.image} alt={article.title} />
+            </AspectRatio>
+            <Text c="dimmed" size="xs" tt="uppercase" fw={700} mt="md">
+              {article.date}
+            </Text>
+            <Text className={classes.title} mt={5}>
+              {article.title}
+            </Text>
+          </Card>
+        )}
+      </Item>
+    </Gallery>
   ));
 
   return (
@@ -61,7 +76,9 @@ function Home() {
       <Title mt={15} className="productheader">
         Olá José Ferreira 👋
       </Title>
-      <SimpleGrid cols={{ base: 1, sm: 2 }} mt={20}>{cards}</SimpleGrid>
+      <SimpleGrid cols={{ base: 1, sm: 2 }} mt={20}>
+        {cards}
+      </SimpleGrid>
     </div>
   );
 }
