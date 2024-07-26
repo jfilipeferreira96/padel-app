695756b2a63edd25b340f4cfdf6b80bce8968337
Filipe Ferreira <filipeferreira96@hotmail.com>
2024-07-26 21:45:18 +0100

    modal-voucher

---
+++ client/src/components/user-modal/modal-voucher.tsx
@@ -0,0 +1,246 @@
+"use client";
+import { useEffect, useState, useCallback } from "react";
+import { Modal, Table, Text, Button, Center, Loader, Paper, Divider, Badge, Tooltip, ActionIcon, Group, TextInput, Box, Flex, Select, rem, Card, Pagination, Radio, CheckIcon } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { notifications } from "@mantine/notifications";
+import { getUser, UserData } from "@/services/user.service";
+import { getAllVouchersHistory, assignVoucher, getAllVouchers } from "@/services/vouchers.service";
+import { IconCheck, IconRefresh, IconSearch } from "@tabler/icons-react";
+import { useSession } from "@/providers/SessionProvider";
+import { usePathname } from "next/navigation";
+
+interface Props {
+  isModalOpen: boolean;
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  userId: number | null;
+  fetchData: () => Promise<void>;
+}
+
+function getBadge(activated_by: number | null) {
+  return activated_by ? { name: "Ativado", color: "green" } : { name: "Não ativado", color: "red" };
+}
+
+interface Voucher {
+  id: number;
+  voucher_name: string;
+  assigned_at: string;
+  assigned_by: number;
+  activated_at: string | null;
+  activated_by: number | null;
+  user_email: string;
+  user_first_name: string;
+  user_last_name: string;
+  phone: string;
+  admin_email: string;
+  admin_first_name: string;
+  admin_last_name: string;
+  reason: string;
+}
+
+export default function ModalVoucher({ isModalOpen, setIsModalOpen, userId, fetchData }: Props) {
+  const { user } = useSession();
+  const pathname = usePathname();
+  const [opened, { open, close }] = useDisclosure(false);
+  const [voucherData, setVouchersData] = useState<{ created_at: "2024-07-25T19:44:59.000Z"; image_url: "./voucher_1h.png"; name: "Voucher 1h"; voucher_id: 1 }[] | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [userVouchers, setUserVouchers] = useState<Voucher[]>([]);
+  const [activePage, setActivePage] = useState<number>(1);
+  const [elementsPerPage, setElementsPerPage] = useState<number>(() => {
+    const storedValue = localStorage.getItem(pathname);
+    return storedValue ? parseInt(storedValue) : 10;
+  });
+  const [totalVouchers, setTotalVouchers] = useState<number>(0);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  useEffect(() => {
+    if (isModalOpen && userId) {
+      fetchUserData(userId);
+      open();
+    } else {
+      close();
+    }
+  }, [isModalOpen, open, close, userId]);
+
+  useEffect(() => {
+    if (!opened) {
+      setIsModalOpen(false);
+      setVouchersData(null);
+      setUserVouchers([]);
+      setIsLoading(true);
+    }
+  }, [opened, setIsModalOpen]);
+
+  const fetchUserData = async (userId: number) => {
+    if (!userId) return;
+
+    try {
+      const pagination = {
+        page: activePage,
+        limit: elementsPerPage,
+        orderBy: "v.voucher_id",
+        order: "DESC",
+      };
+
+      const [vouchers, vouchersResponse] = await Promise.all([getAllVouchers(), getAllVouchersHistory(pagination)]);
+      console.log(vouchers)
+      if (vouchers.status) {
+        //setVouchersData(userResponse.data);
+      }
+      if (vouchersResponse.status) {
+        setUserVouchers(vouchersResponse.data);
+        setTotalVouchers(vouchersResponse.pagination.total || 0);
+        setActivePage(vouchersResponse.pagination.page || 1);
+      }
+      setIsLoading(false);
+    } catch (error) {
+      notifications.show({
+        title: "Erro",
+        message: "Não foi possível carregar os dados",
+        color: "red",
+      });
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (userId) fetchUserData(userId);
+  }, [userId, activePage, elementsPerPage, searchTerm]);
+
+  const handlePageChange = (page: number) => {
+    setActivePage(page);
+  };
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.currentTarget.value);
+  };
+
+  const handleElementsPerPageChange = (value: string | null) => {
+    if (value) {
+      setElementsPerPage(parseInt(value));
+      setActivePage(1);
+      localStorage.setItem(pathname, value);
+    }
+  };
+
+  const onSubmit = async () => {
+    // Implementação da lógica de submissão aqui
+  };
+
+  const initialIndex = (activePage - 1) * elementsPerPage;
+  const finalIndex = initialIndex + elementsPerPage;
+
+  const rows = userVouchers.map((voucher) => (
+    <Table.Tr key={voucher.id}>
+      <Table.Td>
+        <Badge variant="filled" size="md" fw={700} color={getBadge(voucher.activated_by).color} style={{ minWidth: "110px" }}>
+          {getBadge(voucher.activated_by).name}
+        </Badge>
+      </Table.Td>
+      <Table.Td>{voucher.voucher_name}</Table.Td>
+      <Table.Td>
+        {voucher.user_first_name} {voucher.user_last_name}
+      </Table.Td>
+      <Table.Td>{voucher.user_email}</Table.Td>
+      <Table.Td>{voucher.phone}</Table.Td>
+      <Table.Td>{voucher.assigned_at ? `${voucher.admin_first_name} ${voucher.admin_last_name}` : "-"}</Table.Td>
+      <Table.Td>{new Date(voucher.assigned_at).toLocaleString()}</Table.Td>
+      <Table.Td>{voucher.reason ? voucher.reason : "-"}</Table.Td>
+      <Table.Td>{voucher.activated_at ? `${voucher.admin_first_name} ${voucher.admin_last_name}` : "-"}</Table.Td>
+      <Table.Td>{voucher.activated_at ? new Date(voucher.activated_at).toLocaleString() : "-"}</Table.Td>
+      <Table.Td>
+        <Group gap={0} justify="center">
+          {voucher.activated_at ? (
+            <>-</>
+          ) : (
+            <Tooltip label={"Ativar voucher"} withArrow position="top">
+              <ActionIcon variant="subtle" color="green" onClick={() => console.log("123")}>
+                <IconCheck style={{ width: rem(20), height: rem(20) }} stroke={1.5} />
+              </ActionIcon>
+            </Tooltip>
+          )}
+        </Group>
+      </Table.Td>
+    </Table.Tr>
+  ));
+
+  return (
+    <Modal opened={opened} onClose={close} title="Ver/atribuir vouchers" size="xl">
+      {isLoading ? (
+        <Center mt={50} mih={"40vh"}>
+          <Loader color="blue" />
+        </Center>
+      ) : (
+        <>
+          <Group justify="space-between" align="center" mb={"lg"}>
+            <Flex align={"center"}>
+              <Text>A mostrar</Text>
+              <Select
+                data={["10", "20", "30", "50"]}
+                value={elementsPerPage.toString()}
+                allowDeselect={false}
+                style={{ width: "80px", marginLeft: "8px" }}
+                ml={4}
+                mr={4}
+                onChange={(value) => handleElementsPerPageChange(value)}
+              />
+              <Text>vouchers</Text>
+            </Flex>
+          </Group>
+
+          <Table.ScrollContainer minWidth={500}>
+            <Table highlightOnHover>
+              <Table.Thead>
+                <Table.Tr>
+                  <Table.Th>Estado</Table.Th>
+                  <Table.Th>Voucher</Table.Th>
+                  <Table.Th>Nome</Table.Th>
+                  <Table.Th>Email</Table.Th>
+                  <Table.Th>Telefone</Table.Th>
+                  <Table.Th>Atribuido Por</Table.Th>
+                  <Table.Th>Data de Atribuição</Table.Th>
+                  <Table.Th>Razão</Table.Th>
+                  <Table.Th>Ativado Por</Table.Th>
+                  <Table.Th>Data de Ativação</Table.Th>
+                  <Table.Th>Ações</Table.Th>
+                </Table.Tr>
+              </Table.Thead>
+              <Table.Tbody>{rows}</Table.Tbody>
+            </Table>
+          </Table.ScrollContainer>
+
+          {userVouchers.length > 0 && (
+            <Flex justify={"space-between"} mt={"lg"}>
+              <Text>
+                A mostrar {initialIndex + 1} a {Math.min(finalIndex, totalVouchers)} de {totalVouchers} vouchers
+              </Text>
+              <Pagination total={Math.ceil(totalVouchers / elementsPerPage)} onChange={handlePageChange} />
+            </Flex>
+          )}
+
+          <Divider my="md" mt={"lg"} />
+          <h3>Adicionar vouchers</h3>
+
+       {/*    <Select
+              className="specialinput"
+              label="Vouchers disponíveis"
+              placeholder="Selecione um voucher"
+              data={availableLocations.map((loc) => loc.label)}
+              defaultValue={location?.label}
+              onChange={handleLocationChange}
+            />
+ */}
+          <Radio.Group name="is_active" label="Ativo" withAsterisk required mb={"sm"}>
+            <Group mt="xs" defaultValue={"0"}>
+              <Radio value={"1"} label="Sim" icon={CheckIcon} />
+              <Radio value={"0"} label="Não" icon={CheckIcon} defaultChecked checked />
+            </Group>
+          </Radio.Group>
+
+          <Button fullWidth mt="lg" type="submit" onClick={() => onSubmit()}>
+            Guardar
+          </Button>
+        </>
+      )}
+    </Modal>
+  );
+}
