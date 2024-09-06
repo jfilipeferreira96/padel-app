1635bc5d8c9338f802385757b7dd462e0f4a8294
José Filipe Ferreira <jose.ferreira@i-charging.pt>
2024-09-06 15:06:44 +0100

    fix

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -11,7 +11,7 @@ tempo_atual = time.time()
 
 def excluir_arquivos_antigos(diretorio, tempo_limite):
     for arquivo in os.listdir(diretorio):
-        if arquivo.endswith('.,p4'):
+        if arquivo.endswith('.mp4'):
             caminho_arquivo = os.path.join(diretorio, arquivo)
             tempo_modificacao = os.path.getmtime(caminho_arquivo)
     
