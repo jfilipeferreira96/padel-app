a382b508c0d6f418fde0617d40b5f59b0ff07e8b
José Filipe Ferreira <jose.ferreira@i-charging.pt>
2025-06-27 13:37:47 +0100

    first + ' ' + last name fix sql query

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -333,8 +333,23 @@ class UserController {
 
         const searchValue = email || name || phone;
         if (searchValue) {
-          query += ` AND (u.email LIKE ? OR u.phone LIKE ? OR u.first_name LIKE ? OR u.last_name LIKE ?)`;
-          totalCountQuery += ` AND (email LIKE ? OR phone LIKE ? OR first_name LIKE ? OR last_name LIKE ?)`;
+          query += `
+          AND (
+            u.email LIKE ? 
+            OR u.phone LIKE ? 
+            OR u.first_name LIKE ? 
+            OR u.last_name LIKE ?
+            OR CONCAT(u.first_name, ' ', u.last_name) LIKE ?
+          )`;
+        
+        totalCountQuery += `
+          AND (
+            email LIKE ? 
+            OR phone LIKE ? 
+            OR first_name LIKE ? 
+            OR last_name LIKE ?
+            OR CONCAT(first_name, ' ', last_name) LIKE ?
+          )`;
           const searchPattern = `%${searchValue}%`;
           params.push(searchPattern, searchPattern, searchPattern, searchPattern);
         }
